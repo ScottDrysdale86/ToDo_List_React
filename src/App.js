adadf59703c7ff35c93273ddc4bac91ccddaf352
ScottDrysdale86 <sdrysdale86@pm.me>
2022-08-01 15:23:05 +0100

    finished extension

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import { useState } from "react";
 
 function App() {
 	const [tasks, setTasks] = useState([
-		"Buy Shopping",
-		"Clean Bathroom",
-		"Car's MOT",
+		{ name: "Buy Shopping", isPriority: "high" },
+		{ name: "Clean Bathroom", isPriority: "low" },
+		{ name: "Clean Bathroom", isPriority: "high" },
 	]);
 
-	const [newTask, setNewTask] = useState("");
+	const [newTaskName, setNewTaskName] = useState("");
+	const [newTaskPriority, setNewTaskPriority] = useState("");
 
 	const taskNodes = tasks.map((task, index) => {
 		return (
-			<li key={index}>
-				<span>{task}</span>
+			<li key={index} className={task.isPriority}>
+				<span>{task.name}</span>
 			</li>
 		);
 	});
 
-	const handleTaskInput = (event) => {
-		setNewTask(event.target.value);
+	const handleTaskNameInput = (event) => {
+		setNewTaskName(event.target.value);
+	};
+
+	const handleTaskPriorityInput = (event) => {
+		setNewTaskPriority(event.target.value);
 	};
 
 	const saveNewTask = (event) => {
 		event.preventDefault();
 		const copyTasks = [...tasks];
-		copyTasks.push(newTask);
+		copyTasks.push({ name: newTaskName, isPriority: newTaskPriority });
 		setTasks(copyTasks);
-		setNewTask("");
+		setNewTaskName("");
+		setNewTaskPriority("");
 	};
 
 	return (
@@ -35,12 +41,34 @@ function App() {
 			<h1> To Do List</h1>
 			<hr></hr>
 			<form onSubmit={saveNewTask}>
-				<label htmlFor="new-task">Enter Task:</label>
+				<label id="new-task" htmlFor="new-task">
+					Enter Task:
+				</label>
 				<input
 					id="new-task"
 					type="text"
-					// value={newTask}
-					onChange={handleTaskInput}
+					value={newTaskName}
+					onChange={handleTaskNameInput}
+				/>
+				<label id="radio-label-high" htmlFor="priority-high">
+					High
+				</label>
+				<input
+					name="priority"
+					id="priority-high"
+					type="radio"
+					value="high"
+					onChange={handleTaskPriorityInput}
+				/>
+				<label id="radio-label-low" htmlFor="priority-low">
+					Low
+				</label>
+				<input
+					name="priority"
+					id="priority-low"
+					type="radio"
+					value="low"
+					onChange={handleTaskPriorityInput}
 				/>
 				<button type="submit">Submit</button>
 			</form>
